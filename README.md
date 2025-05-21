404f5022f82401f11bbf1082ab16792d3f3cf7b9
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2025-05-21 10:14:26 -0300

    🎉 Begin a project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rick and Morty  Explorer
+
+
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Explorador de Personagens de Rick e Morty
+      </h1>
+    </div>
+  );
+}
+
+export default App;
